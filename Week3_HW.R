d252863e7c33db4e57a9b71b732abbc8720424e7
vbchavali <chavali@usc.edu>
2022-02-01 19:36:57 -0800

    Add files via upload
    Week3 HW

---
+++ Week3_HW.R
@@ -0,0 +1,12 @@
+#practice examples
+quad_form = function(a, b, c){
+  x = (-b + sqrt((b*b) - (4*a*c)))/(2*a)
+  return (x)
+}
+
+quad_form(1,4,4)
+
+#exercise 1.1
+print(attenu)
+missingdata = is.na(attenu['station'])
+print(missingdata)
